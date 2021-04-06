c46431ba930faadd444bd87a2511b52ed229c777
Chad Courtney <Chadcourtney567@gmail.com>
2021-04-06 16:13:32 -0400

    made sliders mobile friendly

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,8 +8,8 @@ $(document).ready(function () {
 
     $('.slider').slider({
         full_width: true,
-        height: 700, // default - height : 400
-        interval: 5000 // default - interval: 6000
+        height: 700,
+        interval: 5000
     });
 
 });
