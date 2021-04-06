3f113f0381e1557b5101abf83273b9b131f3d3ef
Chad Courtney <Chadcourtney567@gmail.com>
2021-04-06 15:54:15 -0400

    updated portfolio page

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function () {
+    $('.sidenav').sidenav();
+});
+
+
+$(document).ready(function () {
+
+    $('.slider').slider({
+        full_width: true,
+        height: 700, // default - height : 400
+        interval: 5000 // default - interval: 6000
+    });
+
+});
